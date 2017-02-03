a2d409ab42d91650c4ee5ec0717cda286d3ba8a2
Ewen Carr <ewencarr@hotmail.com>
2017-02-03 11:25:13 +0000

    increase pets sold

--- week_01/weekend_homework/pet_shop/pet_shop.rb
+++ week_01/weekend_homework/pet_shop/pet_shop.rb
@@ -15,3 +15,9 @@ end
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold] 
 end
+
+def increase_pets_sold(pet_shop, value)
+  sale = pet_shop[:admin][:pets_sold]
+  sale += value
+  pet_shop[:admin][:pets_sold] = sale
+end
